36bda0f0659eb89dabf71a2c490395c7085fcc8b
RobotoSWAG <74079951+RobotoSWAG@users.noreply.github.com>
2021-10-22 11:07:02 -0400

    Modelos Concesionaria

---
+++ SQL/src/Revisiones.java
@@ -0,0 +1,68 @@
+public class Revisiones {
+
+    private String codigo;
+    private String filtro;
+    private String aceite;
+    private String frenos;
+
+    private Coches coche;
+
+    public Revisiones(String codigo, String filtro, String aceite, String frenos, Coches coche) {
+        this.setCodigo(codigo);
+        this.setFiltro(filtro);
+        this.setAceite(aceite);
+        this.setFrenos(frenos);
+        this.setCoche(coche);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(String filtro) {
+        this.filtro = filtro;
+    }
+
+    public String getAceite() {
+        return aceite;
+    }
+
+    public void setAceite(String aceite) {
+        this.aceite = aceite;
+    }
+
+    public String getFrenos() {
+        return frenos;
+    }
+
+    public void setFrenos(String frenos) {
+        this.frenos = frenos;
+    }
+
+    public Coches getCoche() {
+        return coche;
+    }
+
+    public void setCoche(Coches coche) {
+        this.coche = coche;
+    }
+
+    @Override
+    public String toString() {
+        return "Revisiones{" +
+                "codigo='" + codigo + '\'' +
+                ", filtro='" + filtro + '\'' +
+                ", aceite='" + aceite + '\'' +
+                ", frenos='" + frenos + '\'' +
+                ", coche=" + coche +
+                '}';
+    }
+}
